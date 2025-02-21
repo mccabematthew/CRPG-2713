e9274501380088647c38e9059f1f5030271c5c66
mccabematthew <matthew.james.mccabe@outlook.com>
2025-02-20 22:15:42 -0600

    created header per project instructions. Used typedef to prevent using enum for every var dec and per c conventions

---
+++ include/functions.h
@@ -0,0 +1,25 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Player character types
+typedef enum {
+    MAGE,
+    FIGHTER,
+    TANK
+} CHAMPION;
+
+// Function prototypes
+
+CHAMPION createChampion ( );
+
+int getRandomNumber ( int min, int max );
+
+int getChampionPoints ( CHAMPION c );
+
+void printChampion ( CHAMPION c );
+
+void playRound ( int* p1_total, int* p2_total, CHAMPION c1, 
+    int c1_points, CHAMPION c2, int c2_points);
+
+
+#endif
