11ed3c68cb6455ae9ad84a80a04136f1876337ac
mccabematthew <matthew.james.mccabe@outlook.com>
2025-02-24 15:55:41 -0600

    implemented a bunch of createChamp logic, WIP:

--- include/functions.h
+++ include/functions.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <stdio.h>
 
 // Player character types
 typedef enum {
--- src/functions.c
+++ src/functions.c
@@ -1,8 +1,37 @@
 #include "functions.h"
-#include <random>
+#include <stdlib.h>
 
 // TODO: Write Logic
-CHAMPION createChampion ( );
+CHAMPION createChampion ( ) {
+    int championPoints = 0;
+    int championNum = rand() % 3;
+    CHAMPION playerChampion = championNum;
+
+    if (playerChampion == 0) {
+        playerChampion = MAGE;
+        championPoints = rand() % (8 - 1 + 1) + 1;
+    }
+    else if (playerChampion == 1) {
+        playerChampion = FIGHTER;
+        championPoints = rand() % (7 - 3 + 1) + 3;
+    }
+    else if (playerChampion == 2) {
+        playerChampion = TANK;
+        championPoints = rand() % (9 - 5 + 1) + 5;
+    }
+
+    // if (playerChampion == 0) {
+    //     championPoints = rand() % (8 - 1 + 1) + 1;
+    // }
+    // else if (playerChampion == 1) {
+    //     championPoints = rand() % (7 - 3 + 1) + 3;
+    // }
+    // else if (playerChampion == 2) {
+    //     championPoints = rand() % (9 - 5 + 1) + 5;
+    // }
+
+    return playerChampion;
+}
 
 // Computes random num between min and max
 int getRandomNumber ( int min, int max ) {
